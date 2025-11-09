ec8c7944da58c1c3bd92c136a73e60401424967c
victoriamccabe <victoriamccabe96@gmail.com>
2025-11-08 17:15:22 -0700

    Theatre

---
+++ ASP.NET-MVC-and-Entity-Framework/Files/Models/RentalHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace YourProjectName.Areas.Rental.Models
+{
+
+    // Rental History Model
+    public class RentalHistory
+    {
+        [Key] // Primary Key
+        public int RentalHistoryId { get; set; }
+
+        [Display(Name = "Rental Damage")] // Shows the label "Rental Damage" in the UI
+        public bool RentalDamaged { get; set; }
+
+        [Display(Name = "Damage Incurred")] // Shows the label "Damage Incurred" in the UI
+        public string DamagesIncurred { get; set; }
+
+        public string Rental { get; set; }
+       
+    }
+}
